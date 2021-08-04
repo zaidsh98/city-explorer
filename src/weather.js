b9e272251354584069591fc40ba040f8d45e763e
zaidsh98 <sha7rouri29@gmail.com>
2021-08-04 12:11:17 +0300

    final08

---
+++ src/weather.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+
+
+
+class Weather extends React.Component {
+
+  render() {
+    return (
+      <>
+
+        {
+          this.props.showCard &&
+                    this.props.weather.map(element => {
+                      return (<Card style={{ border: 'groove', width: '500px' }} >
+                        <Card.Header as="h5">
+                          {element.date}
+                        </Card.Header>
+                        <Card.Header as="h5">
+                          {element.description}
+                        </Card.Header>
+                      </Card>
+                      );
+                    })
+        }
+
+
+      </>
+    );
+  }
+
+
+}
+
+export default Weather;
